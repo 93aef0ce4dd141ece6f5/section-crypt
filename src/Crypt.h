2fd9a1caa0086ca6d7041028ecbe797f9295c14f
93aef0ce4dd141ece6f5 <asdfhui@gmail.com>
2016-02-22 17:02:20 +1100

    added license info

--- src/Crypt.h
+++ src/Crypt.h
@@ -1,3 +1,26 @@
+/*
+ *   Author           : 93aef0ce4dd141ece6f5
+ *   Title            : Crypt.h
+ *   Description      : function prototypes
+ *
+ * 
+ *   Copyright (C) 2016  93aef0ce4dd141ece6f5
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #define MAX_SECTION_NAME 8
 
 // main.c
